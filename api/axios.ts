1582beca32ff29667826deeeca0dfaa91dd3ca39
handwoong <handwoong@gmail.com>
2025-02-13 04:36:21 +0900

    feat: axios 및 react-query 사전 구성

---
+++ api/axios.ts
@@ -0,0 +1,13 @@
+import axios from 'axios'
+import JSONBig from 'json-bigint'
+
+export const axiosInstance = axios.create({
+  baseURL: process.env.EXPO_PUBLIC_SERVER_URL,
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept-Language': 'ko',
+  },
+  withCredentials: true,
+  transformRequest: data => JSONBig.stringify(data),
+  transformResponse: data => JSONBig.parse(data),
+})
