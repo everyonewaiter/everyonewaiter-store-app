583c3dde5e6b542936cb381f70d69ad57b7b654a
handwoong <handwoong@gmail.com>
2025-02-14 19:28:13 +0900

    refactor: api 서버 주소 환경 변수명 변경

--- api/axios.ts
+++ api/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import JSONBig from 'json-bigint'
 
 export const axiosInstance = axios.create({
-  baseURL: process.env.EXPO_PUBLIC_SERVER_URL,
+  baseURL: process.env.EXPO_PUBLIC_API_SERVER_URL,
   headers: {
     'Content-Type': 'application/json',
     'Accept-Language': 'ko',
