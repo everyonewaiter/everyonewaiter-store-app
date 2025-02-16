5ab205a524199ee33358efbe3ff4910df336cb90
handwoong <handwoong@gmail.com>
2025-02-16 19:28:45 +0900

    MDHU-25 feat: 인풋 컴포넌트 구현 (#2)

---
+++ components/InputLabel.tsx
@@ -0,0 +1,28 @@
+import { StyleSheet, Text } from 'react-native'
+
+import { colors, fonts } from '@/constants'
+
+interface InputLabelProps {
+  label: string
+  disabled?: boolean
+}
+
+const InputLabel = ({ label, disabled = false }: InputLabelProps) => {
+  return (
+    <Text style={[styles.label, disabled && styles.disabled]}>{label}</Text>
+  )
+}
+
+const styles = StyleSheet.create({
+  label: {
+    fontFamily: fonts.PRETENDARD_REGULAR,
+    fontSize: 13,
+    color: colors.GRAY1_33,
+    marginBottom: 6,
+  },
+  disabled: {
+    color: colors.GRAY3_99,
+  },
+})
+
+export default InputLabel
