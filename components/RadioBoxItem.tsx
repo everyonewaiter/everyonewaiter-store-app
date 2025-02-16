c9b7e5ce0f87809e2b5df1fa117eddaf1c2e236f
handwoong <handwoong@gmail.com>
2025-02-17 00:05:20 +0900

    MDHU-25 feat: 라디오 박스 컴포넌트 구현 (#2)

---
+++ components/RadioBoxItem.tsx
@@ -0,0 +1,46 @@
+import { Pressable, PressableProps, StyleSheet, Text } from 'react-native'
+
+import { colors, fonts } from '@/constants'
+
+interface RadioBoxItemProps extends PressableProps {
+  label: string
+  selected: boolean
+}
+
+const RadioBoxItem = ({ label, selected, ...props }: RadioBoxItemProps) => {
+  return (
+    <Pressable
+      style={[styles.container, selected && styles.selectedContainer]}
+      {...props}
+    >
+      <Text style={[styles.label, selected && styles.selectedLabel]}>
+        {label}
+      </Text>
+    </Pressable>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: 111,
+    height: 115,
+    borderWidth: 1,
+    borderColor: colors.GRAY5_E7,
+    borderRadius: 16,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  selectedContainer: {
+    borderColor: colors.PRIMARY_RED,
+  },
+  label: {
+    fontFamily: fonts.PRETENDARD_MEDIUM,
+    fontSize: 16,
+    fontWeight: '500',
+  },
+  selectedLabel: {
+    color: colors.PRIMARY_RED,
+  },
+})
+
+export default RadioBoxItem
