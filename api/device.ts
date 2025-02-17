e92bfb0c2cf12d2e23569e689a58ef310ccaab99
handwoong <handwoong@gmail.com>
2025-02-18 05:53:24 +0900

    MDHU-26 feat: 기기 등록 기능 구현 (#3)

--- api/device.ts
+++ api/device.ts
@@ -1,10 +1,35 @@
 import { axiosInstance } from '@/api/axios'
-import { storageKeys } from '@/constants'
-import { Device } from '@/types'
+import { DevicePurpose, storageKeys } from '@/constants'
+import { Device, valueOf } from '@/types'
 import { getItemOrElseThrow } from '@/utils'
 
 export const getDevice = async (): Promise<Device> => {
   const deviceId = await getItemOrElseThrow<string>(storageKeys.DEVICE_ID)
   const { data } = await axiosInstance.get(`/devices/${deviceId}`)
+  return data
+}
+
+type CreateDeviceRequest = {
+  userId: bigint
+  storeId: bigint
+  name: string
+  tableNo: number
+  purpose: valueOf<typeof DevicePurpose>
+}
+
+type CreateDeviceResponse = {
+  id: bigint
+  secretKey: string
+}
+
+export const createDevice = async ({
+  userId,
+  storeId,
+  ...requestBody
+}: CreateDeviceRequest): Promise<CreateDeviceResponse> => {
+  const { data } = await axiosInstance.post(
+    `/users/${userId}/stores/${storeId}/devices`,
+    requestBody,
+  )
   return data
 }
--- app/(auth)/index.tsx
+++ app/(auth)/index.tsx
@@ -1,9 +1,41 @@
 import { Text, View } from 'react-native'
 
+import { queryClient } from '@/api'
+import Button from '@/components/Button'
+import { queryKeys, storageKeys } from '@/constants'
+import { Device } from '@/types'
+import { getItem, removeItem } from '@/utils'
+
 const Index = () => {
+  const printStorage = async () => {
+    console.log('Device', await getItem<Device>(storageKeys.DEVICE))
+    console.log('DeviceId', await getItem<string>(storageKeys.DEVICE_ID))
+    console.log('SecretKey', await getItem<string>(storageKeys.SECRET_KEY))
+    console.log('StoreId', await getItem<string>(storageKeys.STORE_ID))
+    console.log('UserId', await getItem<string>(storageKeys.USER_ID))
+  }
+
+  const handleSignOut = async () => {
+    await Promise.all([
+      removeItem(storageKeys.DEVICE),
+      removeItem(storageKeys.DEVICE_ID),
+      removeItem(storageKeys.SECRET_KEY),
+      removeItem(storageKeys.STORE_ID),
+      removeItem(storageKeys.USER_ID),
+    ]).then(() => {
+      queryClient.invalidateQueries({
+        queryKey: [queryKeys.DEVICE, queryKeys.GET_DEVICE],
+      })
+    })
+  }
+
   return (
     <View>
       <Text>홈</Text>
+      <View style={{ marginVertical: 20 }}>
+        <Button label={'정보 출력'} onPress={printStorage} />
+      </View>
+      <Button label={'인증 해제'} onPress={handleSignOut} />
     </View>
   )
 }
